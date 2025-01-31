13b64f9ee1aad2f8556ba860b80320a510fc4e63
alexandrov5433 <alexandrov5433@gmail.com>
2025-01-31 18:17:49 +0100

    Middleware for session removed.

---
+++ tempBin/middleware.ts
@@ -0,0 +1,18 @@
+// import { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import { validateJWT } from "./lib/util/jwt";
+
+// export default async function sessionChecker(req: NextRequest) {
+//     try {
+//         const sessionCookie = req.cookies.get('session')?.toString() || '';
+//         if (!sessionCookie) {
+//             throw new Error(); //continue routing
+//         }
+//         const jwtPayload = await validateJWT(sessionCookie);
+//         (req as any).session = jwtPayload instanceof Error ? null : jwtPayload;
+//         return NextResponse.next();
+//     } catch (err) {
+//         (req as any).session = null;
+//         return NextResponse.next();
+//     }
+// }
