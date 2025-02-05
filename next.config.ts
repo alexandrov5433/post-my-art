cdaafa3b4093264be5f4bbf27e073d94d5ebb1be
alexandrov5433 <alexandrov5433@gmail.com>
2025-02-05 17:21:04 +0100

    Allow Image to display images directly from Blob storage.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // "This will allow you to use next/image to display images from your Vercel Blob store."" - (https://vercel.com/docs/storage/vercel-blob/server-upload)
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'my-blob-store.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
   async redirects() {
     return [
       {
