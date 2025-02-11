6fbef2247591b8898e70d7e2190b08fbb3f5b58f
alexandrov5433 <alexandrov5433@gmail.com>
2025-02-11 16:12:29 +0100

    Styling syntax correction.

--- src/ui/addArt/addArt.tsx
+++ src/ui/addArt/addArt.tsx
@@ -1,12 +1,14 @@
 'use client';
-import { useState, useRef, use, Suspense, useEffect, useActionState } from 'react';
+import { useState, useRef, Suspense, useEffect, useActionState, useContext } from 'react';
+
+import { useRouter } from 'next/navigation';
 
 import { UserData, ArtUploadData } from '@/lib/definitions';
 import styles from '@/ui/addArt/add-art.module.css';
 import Button from '../button/button';
 
 import { uploadArtFile } from '@/lib/actions/file';
-import { PutBlobResult } from '@vercel/blob';
+import { PopupMessageContext } from '../popupMessage/popupMessageContext';
 
 const FILE_SIZE_LIMIT: number = 2100000 //2MB (2097152) rounded up
 
@@ -24,6 +26,9 @@ export default function AddArt({
 
     const formRef = useRef(null);
     const uploadedFileHiddenInputRef = useRef(null);
+
+    const messageContext = useContext(PopupMessageContext);
+    const router = useRouter();
 
     const calcCharsLeft = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputType = (e.nativeEvent as InputEvent).inputType;
@@ -77,18 +82,32 @@ export default function AddArt({
         }
     }
 
-    const [state, formAction, isPending] = useActionState(uploadArtFile, { error: '' });
+    const [state, formAction, isPending] = useActionState(uploadArtFile, { success: null, error: '' });
+    console.log(state);
+
+    useEffect(() => {
+        console.log(state);
+        if (state.success) {
+            messageContext.setMessageData({
+                duration: 3000,
+                isShown: true,
+                text: 'Art piece uploaded!',
+                type: 'success'
+            });
+            router.push('/home');
+        }
+    }, [state]);
 
     return (
         <main className={`${styles.mainWrapper}`}>
 
-            <h1>Upload Your Art</h1>
+            <h1 className={`${styles.title}`}>Upload Your Art</h1>
             <form ref={formRef} className={`${styles.uploadForm}`} action={formAction}>
 
                 <div className={`${styles.inputSection}`}>
                     <label htmlFor="name">Name</label>
                     <input type="text" placeholder='Enter the name of your Art' id='name' name='name' onChange={nameInputOnChangeListeners} className={`${styles.inputElement} ${nameError ? styles.falseInput : ''}`} />
-                    <p>Characters left: {charsLeft}</p>
+                    <p className={`${styles.generalParagraph}`}>Characters left: {charsLeft}</p>
                     {
                         nameError ?
                             <p className={`${styles.errorMessage}`}>Name is missing!</p>
@@ -112,7 +131,7 @@ export default function AddArt({
                     <label htmlFor="artFile" className={`${styles.artSecondLabel}`}>Select File</label>
                     {/* hidden */}
                     <input type="file" id='artFile' name='artFile' className={`${styles.artFileInput}`} onChange={fileCheckAndDisplayFileName} ref={uploadedFileHiddenInputRef} />
-                    <input type="text" name='userID' value={userData.userID} className={`${styles.artFileInput}`} readOnly/>
+                    <input type="text" name='userID' value={userData.userID} className={`${styles.artFileInput}`} readOnly />
                     {/* hidden */}
                     <input type="text" className={`${styles.inputElement} ${fileError ? styles.falseInput : ''}`} readOnly value={selectedFileNameState} />
                     {
@@ -132,15 +151,13 @@ export default function AddArt({
                 }
 
                 <div className={`${styles.submitButtonContainer}`}>
-                    <Suspense fallback={<p>Uploading Art...</p>}>
-                        <Button
-                            type='submit'
-                            stylingType='generic'
-                            title='Upload Art'
-                            isLoading={isPending}
-                            disabled={nameError || fileError || nameUntouched || fileUntouched}
-                        ></Button>
-                    </Suspense>
+                    <Button
+                        type='submit'
+                        stylingType='generic'
+                        title='Upload Art'
+                        isLoading={isPending}
+                        disabled={nameError || fileError || nameUntouched || fileUntouched}
+                    ></Button>
                 </div>
 
             </form>
